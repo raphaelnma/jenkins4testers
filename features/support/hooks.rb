0bb867ee4ca46a5be6d46c7f69cbc76d52d5fc7d
Raphael Mantilha <raphael.mantilha@gmail.com>
2019-08-21 16:52:47 -0300

    adicionando os  testes automatizados

---
+++ features/support/hooks.rb
@@ -0,0 +1,21 @@
+Before do
+  @login_page = LoginPage.new
+  @movie_page = MoviePage.new
+  @sidebar = SideBarView.new
+
+  page.current_window.resize_to(1440,900)
+end
+
+Before("@login") do
+  user = CONFIG["users"]["cat_manager"]
+  @login_page.go
+  @login_page.with(user["email"],user["pass"])
+end
+
+After do |scenario|
+  #if scenario.failed?
+    temp_shot = page.save_screenshot("log/temp_shot.png")
+    screenshot = Base64.encode64(File.open(temp_shot, "rb").read)
+    embed(screenshot,"image/png","Screenshot")
+  #end
+end
---
+++ features/support/pages/views/movie_add.rb
@@ -0,0 +1,49 @@
+class MovieAdd
+    include Capybara::DSL
+
+    def create(movie)
+        find('input[name=title]').set movie["title"]
+
+        select_status(movie["status"]) unless movie["status"].empty?
+
+        find('input[name=year]').set movie["year"]
+        find('input[name=release_date]').set movie["release_date"]
+        
+        add_cast(movie["cast"])
+
+        find('textarea[name=overview]').set movie['overview']
+
+        upload(movie["cover"]) unless movie["cover"].empty?
+        
+        find('#create-movie').click
+    end
+
+    def alert 
+        find(".alert").text
+    end
+
+    def upload(file)
+        cover_file = File.join(Dir.pwd, "features/support/fixtures/cover/" + file)
+
+        # Para converter para o padrão Windows o endereço do arquivo.
+        cover_file = cover_file.tr("/","\\") if OS.windows?
+        
+        Capybara.ignore_hidden_elements = false
+        attach_file("upcover",cover_file)
+        Capybara.ignore_hidden_elements = true
+    end
+
+    def add_cast(cast)
+        actor = find('.input-new-tag')
+        cast.each do |a|
+            actor.set a
+            actor.send_keys :tab
+        end
+    end
+
+    def select_status(status)
+        find('input[placeholder=Status]').click
+        find('.el-select-dropdown__item',text: status).click
+    end
+
+end
